df30c88a68895d30129bc05d714c1ff7975bb707
Spitap <dev@asdrip.fr>
2026-04-28 20:19:07 +0200

    tell idp not to cache BC logout response
    Signed-off-by: Spitap <dev@asdrip.fr>

--- lib/Controller/LoginController.php
+++ lib/Controller/LoginController.php
@@ -980,7 +980,12 @@ class LoginController extends BaseOidcController {
 			$this->sessionMapper->delete($oidcSession);
 		}
 
-		return new JSONResponse([], Http::STATUS_OK);
+		// Tell the Idp not to cache the response
+		// Per RFC : https://openid.net/specs/openid-connect-backchannel-1_0.html#BCResponse
+		$response = new JSONResponse([], Http::STATUS_OK);
+		$response->cacheFor(0);
+
+		return $response;
 	}
 
 	/**
@@ -1004,13 +1009,18 @@ class LoginController extends BaseOidcController {
 		}
 		$this->logger->log($logSeverity, 'Backchannel logout error. ' . $error . ' ; ' . $description,
 						   $metadata);
-		return new JSONResponse(
+
+		$response = new JSONResponse(
 			[
 				'error' => $error,
 				'error_description' => $description,
 			],
 			Http::STATUS_BAD_REQUEST,
 		);
+		// Tell the Idp not to cache the response
+		// Per RFC : https://openid.net/specs/openid-connect-backchannel-1_0.html#BCResponse
+		$response->cacheFor(0);
+		return $response;
 	}
 
 	private function toCodeChallenge(string $data): string {
